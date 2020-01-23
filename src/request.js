152f1bf44e7c7927d04f5733602f247fbd01d619
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-01-23 14:28:54 +0000

    code for fetching api

--- src/request.js
+++ src/request.js
@@ -1,12 +1,18 @@
-const getPriceData = async () => {
-    const response = await fetch('https://5e17a1c9505bb50014720dc6.mockapi.io/pricetestapi/prices')
-    
-    if (response.status === 200) {
-        const data = await response.json()
-        return data
-    } else {
-        throw new Error('Unable to get price data from API')
-    }
-}
+const fetch = require("node-fetch");
+const priceApi = "https://5e17a1c9505bb50014720dc6.mockapi.io/pricetestapi/prices"
 
-const priceInfo = await getPriceData()
+const fetchPrices = () => {
+    fetch(priceApi)
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error("Error");
+        }
+  
+        return response.json();
+      })
+      .then(data => console.log("useFetch", data))
+      .catch(e => console.log(e));
+  };
+
+  fetchPrices();
+
